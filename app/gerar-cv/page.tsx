07878af4e327ac947c4d71d7f514ee42e8b255f2
ronaldsalkes-code <ronaldsalkes@gmail.com>
2026-01-31 20:40:10 -0300

    Create page.tsx

---
+++ app/gerar-cv/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Gerador de Currículo</h1>
+      <p>Se você está vendo isso, deu certo.</p>
+    </div>
+  )
+}
